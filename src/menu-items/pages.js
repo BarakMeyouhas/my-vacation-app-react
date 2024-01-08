41857d4a4b12a084490f2ca52374038b0c0bb7b7
Barak1998 <barakm25@gmail.com>
2024-01-08 16:11:43 +0200

    update logout from menu 2

--- src/menu-items/pages.js
+++ src/menu-items/pages.js
@@ -1,41 +1,36 @@
-import { useNavigate } from 'react-router-dom';
+// assets
 import { LogoutOutlined, ProfileOutlined } from '@ant-design/icons';
 
+// icons
 const icons = {
   LogoutOutlined,
   ProfileOutlined
 };
 
-const Menu = () => {
-  const navigate = useNavigate(); // Initialize useNavigate hook here
-
-  const handleLogout = () => {
-    localStorage.removeItem('user');
-    localStorage.removeItem('user_id');
-    localStorage.removeItem('user_password');
-    localStorage.removeItem('user_email');
-    localStorage.removeItem('user_name');
-
-    navigate('/login'); // Use navigate to redirect to /login
-  };
-
-  const pages = {
-    id: 'authentication',
-    type: 'group',
-    children: [
-      {
-        id: 'logout',
-        title: 'Logout',
-        type: 'item',
-        url: '/login',
-        icon: icons.LogoutOutlined,
-        target: '_self',
-        onClick: handleLogout(),
-      }
-    ]
-  };
-
-  
+const handleLogout = () => {
+  localStorage.removeItem('user');
+  localStorage.removeItem('user_id');
+  localStorage.removeItem('user_password');
+  localStorage.removeItem('user_email');
+  localStorage.removeItem('user_name');
 };
 
-export default Menu;
+// ==============================|| MENU ITEMS - EXTRA PAGES ||============================== //
+
+const pages = {
+  id: 'authentication',
+  // title: 'Authentication',
+  type: 'group',
+  children: [
+    {
+      id: 'logout',
+      title: 'Logout',
+      type: 'item',
+      icon: icons.LogoutOutlined,
+      target: '_self',
+      onClick: handleLogout(), // Pass the function reference without invoking it
+    }
+  ]
+};
+
+export default pages;
